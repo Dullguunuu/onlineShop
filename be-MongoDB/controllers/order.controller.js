92aa51cdaeb0f2a86b1af7a374c9e6bd32d310e6
Dullguunuu <dulguun.nst@gmail.com>
2023-03-23 12:26:43 +0800

    mar23

--- be-MongoDB/controllers/order.controller.js
+++ be-MongoDB/controllers/order.controller.js
@@ -2,7 +2,7 @@ const Order = require("../models/order.model")
 
 exports.getAll = async (req, res) => {
     try {
-        const getAllOrder = await Order.find({}).populate("customerId").populate("productId");
+        const getAllOrder = await (await (await Order.find({})).populate("customerId")).populate("productId");
         res.json({ status: true, result: getAllOrder });
     } catch (err) {
         res.json({ status: false, message: err });
@@ -12,7 +12,7 @@ exports.getAll = async (req, res) => {
 exports.getOne = async (req, res) => {
     const { _id } = req.params;
     try {
-        const getOneOrder = await Order.findById({ _id }).populate("customerId").populate("productId");
+        const getOneOrder = await (await (await Order.findById({ _id })).populate("customerId")).populate("productId");
         res.json({ status: true, result: getOneOrder });
     } catch (err) {
         res.json({ status: false, message: err });
@@ -21,7 +21,7 @@ exports.getOne = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
-        const createdOrder = await Order.create(req.body).populate("customerId").populate("productId");
+        const createdOrder = await (await (await Order.create(req.body)).populate("customerId")).populate("productId");
         res.json({ status: true, result: createdOrder });
     } catch (err) {
         res.json({ status: false, message: err });
@@ -31,7 +31,7 @@ exports.create = async (req, res) => {
 exports.update = async (req, res) => {
     const { _id } = req.params;
     try {
-        const updatedOrder = await Order.findByIdAndUpdate({ _id }, req.body, { new: true, }).populate("customerId").populate("productId");
+        const updatedOrder = await (await (await Order.findByIdAndUpdate({ _id }, req.body, { new: true, })).populate("customerId")).populate("productId");
         res.json({ status: true, result: updatedOrder });
     } catch (err) {
         res.json({ status: false, message: err });
--- be-MongoDB/controllers/product.controller.js
+++ be-MongoDB/controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require("../models/product.model")
 
 exports.getAll = async (req, res) => {
     try {
-        const getAllProduct = await Product.find({}).populate("categoryId").populate("brandId").populate("createdAdmin");
+        const getAllProduct = await (await (await (await Product.find({}).populate("categoryId"))).populate("brandId")).populate("createdAdmin");
         res.json({ status: true, result: getAllProduct });
     } catch (err) {
         res.json({ status: false, message: err });
@@ -12,7 +12,7 @@ exports.getAll = async (req, res) => {
 exports.getOne = async (req, res) => {
     const { _id } = req.params;
     try {
-        const getOneProduct = await Product.findById({ _id }).populate("categoryId").populate("brandId").populate("createdAdmin");
+        const getOneProduct = await (await (await (await Product.findById({ _id }).populate("categoryId"))).populate("brandId")).populate("createdAdmin");
         res.json({ status: true, result: getOneProduct });
     } catch (err) {
         res.json({ status: false, message: err });
@@ -21,9 +21,10 @@ exports.getOne = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
-        const createdProduct = await Product.create(req.body, { new: true }).populate("categoryId").populate("brandId").populate("createdAdmin");
+        const createdProduct = await (await (await (await Product.create(req.body)).populate("categoryId")).populate("brandId")).populate("createdAdmin");
         res.json({ status: true, result: createdProduct });
     } catch (err) {
+        console.log(err)
         res.json({ status: false, message: err });
     }
 };
@@ -31,7 +32,7 @@ exports.create = async (req, res) => {
 exports.update = async (req, res) => {
     const { _id } = req.params;
     try {
-        const updatedProduct = await Product.findByIdAndUpdate({ _id }, req.body, { new: true, }).populate("categoryId").populate("brandId").populate("createdAdmin");
+        const updatedProduct = await (await (await (await Product.findByIdAndUpdate({ _id }, req.body, { new: true, })).populate("categoryId")).populate("brandId")).populate("createdAdmin");
         res.json({ status: true, result: updatedProduct });
     } catch (err) {
         res.json({ status: false, message: err });
